54c7c9e81903e403db6f356ab15488f8ee470dcd
bibizio <fabrizio.giovinazzi@gmail.com>
2021-12-15 18:54:27 +0100

    add DialogInput

--- src/components/index.js
+++ src/components/index.js
@@ -4,3 +4,4 @@ export { default as Prompt } from "./Prompt.svelte";
 export { default as Dialog } from "./Dialog.svelte";
 export { default as DialogCore } from "./DialogCore.svelte";
 export { default as DialogContent } from "./DialogContent.svelte";
+export { default as DialogInput } from "./DialogInput.svelte";
